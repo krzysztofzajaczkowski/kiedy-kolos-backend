86fc2b289e44e23db3b6705640253309241ce741
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-02-15 21:41:42 +0100

    Add custom Result and generic Result classes

---
+++ KiedyKolos/Common/KiedyKolos.Core/Result/Result.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace KiedyKolos.Core.Result
+{
+    public enum ResultType
+    {
+        Ok,
+        Created,
+        Updated,
+        Deleted
+    }
+
+    public enum ErrorType
+    {
+        NotFound,
+        NotValid,
+        NotAuthenticated,
+        NotAuthorized,
+        Unknown
+    }
+
+    public class Result
+    {
+        public bool Succeeded { get; set; }
+        public List<string> ErrorMessages { get; set; } = new List<string>();
+        public ResultType? ResultType { get; set; }
+        public ErrorType? ErrorType { get; set; }
+
+        public Result()
+        {
+            Succeeded = true;
+            ResultType = Core.Result.ResultType.Ok;
+        }
+
+        public static Result Success(ResultType resultType)
+        {
+            return new Result
+            {
+                Succeeded = true,
+                ResultType = resultType
+            };
+        }
+
+        public static Result Fail(ErrorType errorType)
+        {
+            return new Result
+            {
+                Succeeded = false,
+                ErrorType = errorType
+            };
+        }
+
+        public static Result Fail(ErrorType errorType, List<string> errors)
+        {
+            return new Result
+            {
+                Succeeded = false,
+                ErrorType = errorType,
+                ErrorMessages = errors
+            };
+        }
+
+    }
+
+    public class Result<T> : Result
+    {
+        public T Output { get; set; }
+
+        public static Result Success(ResultType resultType, T output)
+        {
+            return new Result<T>
+            {
+                Succeeded = true,
+                ResultType = resultType,
+                Output = output
+            };
+        }
+    }
+}
