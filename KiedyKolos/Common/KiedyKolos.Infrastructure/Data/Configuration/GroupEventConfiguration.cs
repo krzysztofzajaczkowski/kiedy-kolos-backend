b72693acfc658ebbaad769cfd6c3d7b76cc28b70
JakubK <jakubkuniszewski1@gmail.com>
2021-02-15 13:10:57 +0100

    Create Configuration class containing relationships and constrains for each entity

---
+++ KiedyKolos/Common/KiedyKolos.Infrastructure/Data/Configuration/GroupEventConfiguration.cs
@@ -0,0 +1,22 @@
+using KiedyKolos.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace KiedyKolos.Infrastructure.Data.Configuration
+{
+  public class GroupEventConfiguration : IEntityTypeConfiguration<GroupEvent>
+  {
+    public void Configure(EntityTypeBuilder<GroupEvent> builder)
+    {
+      builder.HasKey(e => e.Id);
+
+      builder.HasOne<Event>(e => e.Event)
+        .WithMany(e => e.GroupEvents)
+        .HasForeignKey(e => e.EventId);
+
+      builder.HasOne<Group>(e => e.Group)
+        .WithMany(e => e.GroupEvents)
+        .HasForeignKey(e => e.GroupId);
+    }
+  }
+}
