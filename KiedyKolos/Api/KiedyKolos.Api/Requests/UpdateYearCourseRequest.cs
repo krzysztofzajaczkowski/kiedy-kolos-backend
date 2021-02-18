ff7d0298e49c908dc753c865bb2c5edec5582406
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-02-18 15:04:54 +0100

    Add basic requests and responses

---
+++ KiedyKolos/Api/KiedyKolos.Api/Requests/UpdateYearCourseRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KiedyKolos.Api.Requests
+{
+    public class UpdateYearCourseRequest
+    {
+        public int Id { get; set; }
+        public string Course { get; set; }
+        public int CourseStartYear { get; set; }
+        public string Faculty { get; set; }
+        public string University { get; set; }
+        public int CurrentSemester { get; set; }
+    }
+}
