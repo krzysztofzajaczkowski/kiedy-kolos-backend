b66a73636ffd24d0af4728b8f678a06e83fc755d
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-02-18 15:03:34 +0100

    Add MappingProfile configuration

---
+++ KiedyKolos/Api/KiedyKolos.Api/Configuration/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using KiedyKolos.Api.Requests;
+using KiedyKolos.Api.Responses;
+using KiedyKolos.Core.Models;
+
+namespace KiedyKolos.Api.Configuration
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+        }
+    }
+}
