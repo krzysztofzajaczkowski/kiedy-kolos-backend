927a37d8161d846848d95bd9ae82433b7758b1f2
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-02-18 15:04:11 +0100

    Add generic ApiResponse

---
+++ KiedyKolos/Api/KiedyKolos.Api/Responses/ApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KiedyKolos.Api.Responses
+{
+    public class ApiResponse<T> where T: class
+    {
+        public List<string> Messages { get; set; } = new List<string>();
+        public T Result { get; set; }
+    }
+}
