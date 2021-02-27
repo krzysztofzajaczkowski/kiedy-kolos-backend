ecb12736ac1029b6716b56efce0865ff2d5dc499
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-02-27 12:17:26 +0100

    Add UpdateYearCourseCommand with handler

---
+++ KiedyKolos/Common/KiedyKolos.Core/Commands/UpdateYearCourseCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KiedyKolos.Core.Middleware.Interfaces;
+using KiedyKolos.Core.Result;
+using MediatR;
+
+namespace KiedyKolos.Core.Commands
+{
+    public class UpdateYearCourseCommand :  IRequest<BaseResult>, IAuthorizable
+    {
+        public int YearCourseId { get; set; }
+        public string Course { get; set; }
+        public int CourseStartYear { get; set; }
+        public string Faculty { get; set; }
+        public string University { get; set; }
+        public int CurrentSemester { get; set; }
+        public string Password { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
