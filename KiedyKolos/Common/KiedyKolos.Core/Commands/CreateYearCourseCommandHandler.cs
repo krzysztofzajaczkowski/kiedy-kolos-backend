d883036c783ff4b65128153cc695177a24204c8c
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-02-27 12:14:36 +0100

    Add CreateYearCourseCommand with handler

---
+++ KiedyKolos/Common/KiedyKolos.Core/Commands/CreateYearCourseCommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using KiedyKolos.Core.Interfaces;
+using KiedyKolos.Core.Models;
+using KiedyKolos.Core.Result;
+using MediatR;
+
+namespace KiedyKolos.Core.Commands
+{
+    public class CreateYearCourseCommandHandler : IRequestHandler<CreateYearCourseCommand, BaseResult<int>>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateYearCourseCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<BaseResult<int>> Handle(CreateYearCourseCommand request, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrEmpty(request.CreationApiKey))
+            {
+                return BaseResult<int>.Fail(ErrorType.NotAuthenticated,
+                    new List<string>
+                    {
+                        "No password provided!"
+                    });
+            }
+
+            if (await _unitOfWork.KeyRepository.TryUseKeyAsync(request.CreationApiKey))
+            {
+                var yearCourse = new YearCourse
+                {
+                    Password = request.Password,
+                    CurrentSemester = request.CurrentSemester,
+                    Course = request.Course,
+                    CourseStartYear = request.CourseStartYear,
+                    Faculty = request.Faculty,
+                    University = request.University
+                };
+                await _unitOfWork.YearCourseRepository.AddAsync(yearCourse);
+
+                await _unitOfWork.CommitAsync();
+                
+                return BaseResult<int>.Success(ResultType.Created, yearCourse.Id);
+            }
+
+            await _unitOfWork.RollbackAsync();
+
+            return BaseResult<int>.Fail(ErrorType.NotAuthorized,
+                new List<string>
+                {
+                    "Invalid creation api key!"
+                });
+        }
+    }
+}
