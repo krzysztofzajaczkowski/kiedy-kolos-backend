854c577c8adbabc3b3195a5feb20ee5b9aed2354
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-03-02 18:08:06 +0100

    Add CreateYearCourseSubject command

---
+++ KiedyKolos/Common/KiedyKolos.Core/Commands/CreateYearCourseSubjectCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Text;
+using KiedyKolos.Core.Middleware.Interfaces;
+using KiedyKolos.Core.Result;
+using MediatR;
+
+namespace KiedyKolos.Core.Commands
+{
+    public class CreateYearCourseSubjectCommand : IRequest<BaseResult<int>>, IAuthorizable
+    {
+        public int YearCourseId { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
