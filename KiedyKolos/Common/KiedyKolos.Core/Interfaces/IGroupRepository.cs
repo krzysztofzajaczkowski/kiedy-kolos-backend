73b19135d8691fad2a0f572bbb34a418d70d4f6d
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-03-06 19:12:46 +0100

    Add Group repository with interface and implementation

---
+++ KiedyKolos/Common/KiedyKolos.Core/Interfaces/IGroupRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KiedyKolos.Core.Models;
+
+namespace KiedyKolos.Core.Interfaces
+{
+    public interface IGroupRepository
+    {
+        Task AddAsync(Group group);
+        Task<List<Group>> GetAllForYearCourseAsync(int yearCourseId);
+        Task<Group> GetByIdAsync(int id);
+        Task UpdateAsync(Group group);
+        Task DeleteAsync(int id);
+        Task<Group> GetYearCourseGroup(int yearCourseId, int groupId);
+    }
+}
