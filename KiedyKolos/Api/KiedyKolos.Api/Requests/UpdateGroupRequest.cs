82cec5dfd37fd1ae36816b584d4b12fb2c9016d2
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-03-06 22:39:30 +0100

    Add UpdateGroupRequest

---
+++ KiedyKolos/Api/KiedyKolos.Api/Requests/UpdateGroupRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KiedyKolos.Api.Requests
+{
+    public class UpdateGroupRequest
+    {
+        public int GroupId { get; set; }
+        public int YearCourseId { get; set; }
+        public int GroupNumber { get; set; }
+        public string GroupName { get; set; }
+    }
+}
