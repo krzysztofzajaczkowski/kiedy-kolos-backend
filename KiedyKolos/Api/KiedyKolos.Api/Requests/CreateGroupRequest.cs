f2c84f86135c486fff0385496daabac80903129a
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-03-06 22:39:15 +0100

    Add YearCourseId property to CreateGroupRequest

--- KiedyKolos/Api/KiedyKolos.Api/Requests/CreateGroupRequest.cs
+++ KiedyKolos/Api/KiedyKolos.Api/Requests/CreateGroupRequest.cs
@@ -7,6 +7,7 @@ namespace KiedyKolos.Api.Requests
 {
     public class CreateGroupRequest
     {
+        public int YearCourseId { get; set; }
         public int GroupNumber { get; set; }
         public string GroupName { get; set; }
     }
