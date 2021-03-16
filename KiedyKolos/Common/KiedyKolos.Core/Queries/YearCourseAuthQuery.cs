72030d3aec01b05ebdfbdbbf401f04eb30a5e312
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-03-16 17:56:37 +0100

    Add endpoint for api key verification

---
+++ KiedyKolos/Common/KiedyKolos.Core/Queries/YearCourseAuthQuery.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using KiedyKolos.Core.Middleware.Interfaces;
+using KiedyKolos.Core.Result;
+using MediatR;
+
+namespace KiedyKolos.Core.Queries
+{
+    public class YearCourseAuthQuery : IRequest<BaseResult>, IAuthorizable
+    {
+        public int YearCourseId { get; set; }
+        public string Password { get; set; }
+    }
+}
