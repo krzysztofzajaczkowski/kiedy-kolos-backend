e1396b30fba46b5c5272b0cbe68f6e231ebab729
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-04-11 17:48:57 +0200

    Add validator for CreateEventTypeCommand

---
+++ KiedyKolos/Common/KiedyKolos.Core/Validators/EventType/CreateEventTypeCommandValidator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+using KiedyKolos.Core.Commands;
+using KiedyKolos.Core.Interfaces;
+
+namespace KiedyKolos.Core.Validators.EventType
+{
+    public class CreateEventTypeCommandValidator : AbstractValidator<CreateEventTypeCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateEventTypeCommandValidator(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+            RuleFor(e => e.Name).MustAsync(async (e, cancellation) =>
+            {
+                var eventTypes = await _unitOfWork.EventTypeRepository.GetAllAsync();
+
+                return !eventTypes.Any(et => et.Name == e);
+            }).WithMessage("Event type with this name already exists!");
+
+            RuleFor(x => x.Name)
+                .NotEmpty().MaximumLength(20)
+                .WithMessage("Event type name must not be empty and should be shorter than 20 characters!");
+        }
+    }
+}
