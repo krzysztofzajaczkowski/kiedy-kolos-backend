1852d160babe326684b03fdd518e96c574754620
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-04-11 14:16:23 +0200

    Add GetEventDetailsResponse

---
+++ KiedyKolos/Api/KiedyKolos.Api/Responses/GetEventDetailsResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KiedyKolos.Api.Responses
+{
+    public class GetEventDetailsResponse
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public int SubjectId { get; set; }
+        public int EventTypeId { get; set; }
+        public int YearCourseId { get; set; }
+        public List<int> GroupIds { get; set; }
+    }
+}
