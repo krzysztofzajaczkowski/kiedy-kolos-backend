62bdeec2bec066b45458e406e1ab9e43eada694b
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-04-12 19:24:59 +0200

    Add validation for inline entities in CreateYearCourseCommand

--- KiedyKolos/Common/KiedyKolos.Core/Validators/YearCourse/CreateYearCourseCommandValidator.cs
+++ KiedyKolos/Common/KiedyKolos.Core/Validators/YearCourse/CreateYearCourseCommandValidator.cs
@@ -43,14 +43,51 @@ namespace KiedyKolos.Core.Validators.YearCourse
                 .WithMessage(
                     $"Password must have at least 8 characters with at least 1 letter and 1 digit!");
 
-            RuleFor(yc => yc.Course).NotEmpty().Matches(@"^[a-zA-Z0-9 ]+$")
+            RuleFor(yc => yc.Course).NotEmpty().Matches(@"^[a-zA-Z0-9 ĄĆĘŁŃÓŚŹŻąćęłńóśźż]+$")
                 .WithMessage($"Course must contain only alphanumeric characters and spaces!");
 
-            RuleFor(yc => yc.Faculty).NotEmpty().Matches(@"^[a-zA-Z0-9 ]+$")
+            RuleFor(yc => yc.Faculty).NotEmpty().Matches(@"^[a-zA-Z0-9 ĄĆĘŁŃÓŚŹŻąćęłńóśźż]+$")
                 .WithMessage($"Faculty must contain only alphanumeric characters and spaces!");
 
-            RuleFor(yc => yc.University).NotEmpty().Matches(@"^[a-zA-Z0-9 ]+$")
+            RuleFor(yc => yc.University).NotEmpty().Matches(@"^[a-zA-Z0-9 ĄĆĘŁŃÓŚŹŻąćęłńóśźż]+$")
                 .WithMessage($"University must contain only alphanumeric characters and spaces!");
+
+            RuleFor(x => x.Subjects).Must(x =>
+            {
+                var shortNames = x.Select(s => s.ShortName).Distinct();
+                var names = x.Select(s => s.Name).Distinct();
+
+                return !(shortNames.Count() != x.Count || names.Count() != x.Count);
+            }).WithMessage("Subject name and short name should be unique in a year course");
+
+            RuleForEach(x => x.Subjects)
+                .ChildRules(x => 
+                    x.RuleFor(s => s.ShortName)
+                        .NotEmpty()
+                        .MaximumLength(6)
+                        .WithMessage("Short name can have maximum 6 characters"))
+                .ChildRules(x => 
+                    x.RuleFor(s => s.Name)
+                        .NotEmpty()
+                        .MaximumLength(30)
+                        .WithMessage("Name can have maximum 30 characters"));
+
+            RuleFor(x => x.Groups).Must(x =>
+            {
+                var groupNames = x.Select(g => g.GroupName).Distinct();
+                var groupNumbers = x.Select(g => g.GroupNumber).Distinct();
+
+                return !(groupNumbers.Count() != x.Count || groupNames.Count() != x.Count);
+            }).WithMessage("Group name and number should be unique in a year course!");
+
+            RuleForEach(x => x.Groups)
+                .ChildRules(x =>
+                    x.RuleFor(g => g.GroupName)
+                        .NotEmpty()
+                        .MaximumLength(20))
+                .ChildRules(x =>
+                    x.RuleFor(g => g.GroupNumber)
+                        .GreaterThanOrEqualTo(1));
 
         }
     }
