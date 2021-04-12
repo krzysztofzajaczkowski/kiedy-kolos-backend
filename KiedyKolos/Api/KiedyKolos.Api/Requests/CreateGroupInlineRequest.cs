8c644c087a125da3c4a725133988b21a3c0cbdfe
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-04-12 19:23:28 +0200

    Add inline request classes

---
+++ KiedyKolos/Api/KiedyKolos.Api/Requests/CreateGroupInlineRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KiedyKolos.Api.Requests
+{
+    public class CreateGroupInlineRequest
+    {
+        public int GroupNumber { get; set; }
+        public string GroupName { get; set; }
+    }
+}
---
+++ KiedyKolos/Api/KiedyKolos.Api/Requests/CreateSubjectInlineRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KiedyKolos.Api.Requests
+{
+    public class CreateSubjectInlineRequest
+    {
+        public string Name { get; set; }
+        public string ShortName { get; set; }
+    }
+}
